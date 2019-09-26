873704f0fe62d7996d0964d211676a44060c8322
Rohan Patil <eyeoeternity@gmail.com>
2019-09-26 23:57:58 +0530

    Creating completely working cd

--- src/cd.rs
+++ src/cd.rs
@@ -1,6 +1,5 @@
 pub fn our_cd(args: &Vec<&str>) -> ()  {
 	use std::{env};
-	use std::string::String;
 	use std::path::Path;
 	let current_dir = env::current_dir().unwrap();
 	let mut path = String::from("");
@@ -11,35 +10,14 @@ pub fn our_cd(args: &Vec<&str>) -> ()  {
 
 	if args[1].chars().next().unwrap() != '/'{
 		path = current_dir.into_os_string().into_string().unwrap();
+		path.push('/');
+		path.push_str(args[1]);
 	}
 	else {
 		assert!(env::set_current_dir(&Path::new(args[1])).is_ok());
 		return
 	}
-	
-	let mut vector_path: Vec<&str> = path.split("/").collect();
-	let _pop = vector_path.pop();
-	
-	if vector_path.len() == 1{
-		let error_path = format!("{}{}",path,"/");
-		let root = Path::new(&error_path);
-		assert!(env::set_current_dir(&root).is_ok());
-		return		
-	}
-	
-	let mut a = vec![];
-	let mut b;
-	for mum in &vector_path {
-		b = format!("{}{}",mum,"/");
-		a.push(b);
-	}
-	let final_string: String = a.into_iter().collect();
-
-	if args[1].trim() == ".." || args[1].trim() == "../"{
-		let root = Path::new(&final_string);
-		assert!(env::set_current_dir(&root).is_ok());
-		
-	}
+	assert!(env::set_current_dir(&Path::new(&path)).is_ok());
 	
 	return
 }
