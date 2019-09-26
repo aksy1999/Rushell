f1152b8836862f8287a6ae6281c47122bdee153e
Rohan Patil <eyeoeternity@gmail.com>
2019-09-26 16:12:58 +0530

    Changes to cd

--- src/cd.rs
+++ src/cd.rs
@@ -5,6 +5,10 @@ pub fn our_cd(args: &Vec<&str>) -> ()  {
 	let current_dir = env::current_dir().unwrap();
 	let mut path = String::from("");
 
+	if args.len() == 1{
+		return
+	}
+
 	if args[1].chars().next().unwrap() != '/'{
 		path = current_dir.into_os_string().into_string().unwrap();
 	}
