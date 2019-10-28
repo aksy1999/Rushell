310affbdcb84e1c033ddf7e173e02dbc92049e53
Rohan Patil <31570118+bridgesign@users.noreply.github.com>
2019-10-29 00:26:14 +0530

    Delete cwd.rs

--- src/cwd.rs
+++
@@ -1,8 +0,0 @@
-// Function to get the current working directory
-pub fn our_cwd(args: &Vec<&str>)-> std::io::Result<()> {
-	use std::env;
-    let path = env::current_dir()?;
-    println!("{}", path.display());
-    //process::exit(code);
-    Ok(())
-}
