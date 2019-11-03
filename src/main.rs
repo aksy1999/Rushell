7ddc99172fbd2f680bc74eed718b0657bc154589
Rohan Patil <eyeoeternity@gmail.com>
2019-11-03 13:50:12 +0530

    for loop implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
+use std::io::{stdin,stdout,Write};
+
 
 mod internal;
 mod parser;
 
-fn run_internal(args: Vec<Vec<String>>, stack: &mut HashMap<String, String>) -> () {
+fn run_internal(args: Vec<Vec<String>>, stack: &mut HashMap<String, String>, internal_commands: &Vec<String>) -> String {
+
+	let mut status = "ok";
+
 	if &args[0][0] == "exit" {
 		internal::our_exit(args[0].clone());
 	}
@@ -11,10 +16,11 @@ fn run_internal(args: Vec<Vec<String>>, stack: &mut HashMap<String, String>) -> 
 		internal::set(String::from("CWD"), internal::our_cd(args[0].clone()).clone(), stack);
 	}
 	if &args[0][0] == "clear" {
-		print!("\x1B[2J");
+		println!("\x1B[2J");
 	}
 	if &args[0][0] == "set" {
 		if args[0].len() != 3{
+			status = "Error";
 			println!("Format is set <variable> <value>");
 		}
 		else {
@@ -26,50 +32,67 @@ fn run_internal(args: Vec<Vec<String>>, stack: &mut HashMap<String, String>) -> 
 	}
 	if &args[0][0] == "unset" {
 		if args[0].len() != 2 {
-			println!("Format is unset <varibale>");;
+			status = "Error";
+			println!("Format is unset <varibale>");
 		}
 		else {
-			internal::unset(args[0][1].clone(), stack);;
+			internal::unset(args[0][1].clone(), stack);
 		}
 	}
+	if &args[0][0] == "for" {
+		if args[0].len() != 4{
+			status = "Error";
+			println!("Format for <var> <lower> <upper>");
+		}
+		else {
+			let lower: i32 = args[0][2].parse().unwrap();
+			let upper: i32  = args[0][3].parse().unwrap();
+			if lower+1 >= upper {
+				status = "Error";
+				return String::from(status);
+			}
+			print!(": ");
+			let mut command = String::from("");
+			let _=stdout().flush();
+			stdin().read_line(&mut command).expect("Error taking Input");
+
+			for i in lower..upper{
+				internal::set(args[0][1].clone(), String::from(i.to_string()), stack);
+				let execute = executer(command.clone(), &internal_commands, stack);
+				if execute == "Error"{
+					break;
+				}
+			}
+			internal::unset(args[0][1].clone(), stack);
+
+		}
+	}
+	String::from(status)
 }
 
-fn main() {
-	use std::io::{stdin,stdout,Write};
-	let mut stack = internal::init_stack();
-	let mut internal_commands = Vec::new();
-	
-	internal_commands.push(String::from("exit"));
-	internal_commands.push(String::from("cd"));
-	internal_commands.push(String::from("clear"));
-	internal_commands.push(String::from("set"));
-	internal_commands.push(String::from("unset"));
-	internal_commands.push(String::from("prompt"));
-
-	loop {
-		let mut command = String::from("");
-		print!("{}",stack["PROMPT"]);
-
-		let _=stdout().flush();
-		stdin().read_line(&mut command).expect("Error taking Input");
-
-		for (key, value) in &stack {
+fn executer(
+	command: String, 
+	internal_commands: &Vec<String>, 
+	stack: &mut HashMap<String, String>
+	) -> String {
+		let mut command = command;
+		for (key, value) in stack.clone() {
 			command = command.replace(&format!("${}", key),&value);
 		}
 
-		command = String::from(command);
+		let command = String::from(command);
 
 		let args_list = parser::parser(command);
+		let mut execute = "external";
 
 		if &args_list[0][0]==""{
-			continue;
+			return String::from(execute);
 		}
-		let mut execute = "external";
 
 		let args_len = args_list.len();
 
 		for i in 0..args_len{
-			for com in &internal_commands {
+			for com in internal_commands {
 				if &&args_list[i][0] == &com{
 					if args_len > 1 {
 						println!("Internal Commands cannot be piped");
@@ -88,11 +111,35 @@ fn main() {
 		}
 
 		if execute=="internal" {	
-			run_internal(args_list.clone(), &mut stack);
+			if &run_internal(args_list.clone(), stack, &internal_commands) == "Error"{
+				execute = "Error";
+			}
 		}
 		if execute=="external" {
 			println!("{:?}", args_list.clone());
 		}
-		
+		String::from(execute)
+}
+
+fn main() {
+	let mut stack = internal::init_stack();
+	let mut internal_commands = Vec::new();
+	
+	internal_commands.push(String::from("exit"));
+	internal_commands.push(String::from("cd"));
+	internal_commands.push(String::from("clear"));
+	internal_commands.push(String::from("set"));
+	internal_commands.push(String::from("unset"));
+	internal_commands.push(String::from("prompt"));
+	internal_commands.push(String::from("for"));
+
+	loop {
+		let mut command = String::from("");
+		print!("{}",stack["PROMPT"]);
+
+		let _=stdout().flush();
+		stdin().read_line(&mut command).expect("Error taking Input");
+
+		executer(command, &internal_commands, &mut stack);
 	}
 }
