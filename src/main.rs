3ea0fea1c4b8af97bd38021d4bb6170ef9fa50a4
Akhilesh Ravi <akhileshravis@gmail.com>
2019-11-07 01:46:00 +0530

    Updated for merge

--- src/main.rs
+++ src/main.rs
@@ -1,220 +1,216 @@
-mod copy;
-mod remove;
-mod remove_single;
-mod cwd;
-mod mkdir;
-mod echo;
-mod cd;
-mod grep;
-mod cat;
-mod ls;
-mod datetime;
-mod mov;
+use std::collections::HashMap;
+use std::io::{stdin,stdout,Write};
 
-fn get_arg(char_vec: &Vec<char>, mut start: usize) -> (usize, usize, usize) {
-	let mut comp = ' ';
-	let mut arg_start = start;
-	if char_vec[start] == '"'{
-		comp = '"';
-		start = start+1;
-		arg_start = start;
-	}
-	while char_vec[start]!=comp {
-		start = start+1;
-	}
-	let arg_stop = start;
-	if comp=='"'{
-		start = start+2;
-	}
-	else {
-		start = start+1;
-	}
-	(start, arg_start, arg_stop)
-}
+mod spawner;
+mod internal;
+mod parser;
 
-fn get_args_len(char_vec: &Vec<char>) -> i32 {
-	let mut len = 0;
-	let mut i = 0;
-	while i < char_vec.len() {
-		if char_vec[i]=='"' {
-			i = i+1;
-			while char_vec[i]!='"' {
-				i=i+1;
-			}
-		}
-		if char_vec[i]==' ' {
-			len = len + 1;
-		}
-		i=i+1;
-	}
-	len
-}
+use std::io::*;
+
+use std::panic;
 
 
-fn start_process(args: &Vec<&str>) -> () {
-	use std::process;
-	use std::panic;
 
-    //This suppresses the panic verbose remove if required.
-	panic::set_hook(Box::new(|_info| {
-        // do nothing
-    }));
+// This function takes care of running internal commands
+fn run_internal(
+	args: Vec<Vec<String>>,
+	stack: &mut HashMap<String, String>,
+	internal_commands: &Vec<String>
+	) -> String {
 
-	let result = panic::catch_unwind(|| {
-    	let mut child = process::Command::new(args[0])
-    							.args(args)
-    							.spawn()
-    							.expect("command failed to start");
-    	let ecode = child.wait()
-    				.expect("failed to wait on child");
-        assert!(ecode.success());
-        });
+	// For noting down the status of internal command
+	let mut status = "ok";
 
-	if result.is_err(){
-		// This will catch any error that occurs during the run of a command.
-		println!("'{}' is not recognized as an internal or extrernal command,\n or operable program", args[0]);
+	// Calls exit command
+	if &args[0][0] == "exit" {
+		internal::our_exit(args[0].clone());
 	}
-
-}
-
-fn our_exit(args: &Vec<&str>) -> () {
-	use std::process;
-	if args.len() == 1{
-		process::exit(0);
+	// Calls cd
+	if &args[0][0] == "cd" {
+		internal::set(String::from("CWD"), internal::our_cd(args[0].clone()).clone(), stack);
 	}
-	else {
-		let code:i32 = args[1].parse::<i32>().unwrap();
-		process::exit(code);
-	}
-}
-
-// pub fn our_move(args: &Vec<&str>) -> (){
-// 	use copy;
-// 	copy::our_copy(&args);
-// 	remove_single::our_remove_single(&args);
-// }
-
-fn run_internal(args: &Vec<&str>) -> () {
-	if args[0] == "exit" {
-		our_exit(&args)
-	}
-	if args[0] == "cd" {
-		cd::our_cd(&args)
-	}
-	if args[0] == "cwd" {
-		cwd::our_cwd(&args);
-	}
-	if args[0] == "echo" {
-		echo::our_echo(&args);
-	}
-	if args[0] == "mkdir" {
-		mkdir::our_mkdir(&args);
-	}
-	if args[0] == "cp" {
-		copy::our_copy(&args);
-	}
-	if args[0] == "rm" {
-		remove::our_remove(&args);
-	}
-	if args[0] == "mv" {
-		mov::our_move(&args);
-	}
-	if args[0] == "grep" {
-		grep::our_grep(&args);
-	}
-	if args[0] == "cat" {
-		cat::our_cat(&args);
-	}
-	if args[0] == "ls" {
-		ls::our_ls(&args);
-	}
-	if args[0] == "date" {
-		datetime::date();
-	}
-	if args[0] == "clear" {
+	// Calls clear (works only in Ubuntu)
+	if &args[0][0] == "clear" {
 		print!("\x1B[2J");
 	}
-	// if args[0] == "sleep" {
-	// 	datetime::sleep();
-	// }
+	// Sets a variable
+	if &args[0][0] == "set" {
+		if args[0].len() != 3{
+			status = "Error";
+			println!("Format is set <variable> <value>");
+		}
+		else {
+			internal::set(args[0][1].clone(), args[0][2].clone(), stack);
+		}
+	}
+	// For setting the prompt
+	if &args[0][0] == "prompt" {
+		internal::set(String::from("PROMPT"), internal::set_prompt(args[0].clone()).clone(), stack);
+	}
+	//Unset a variable
+	if &args[0][0] == "unset" {
+		if args[0].len() != 2 {
+			status = "Error";
+			println!("Format is unset <varibale>");
+		}
+		else {
+			internal::unset(args[0][1].clone(), stack);
+		}
+	}
+	// Runs a for loop
+	if &args[0][0] == "for" {
+		if args[0].len() != 4{
+			status = "Error";
+			println!("Format for <var> <lower> <upper>");
+		}
+		else {
+			//Setting lower and upper limit [a, b)
+			let lower: i32 = args[0][2].parse().unwrap();
+			let upper: i32  = args[0][3].parse().unwrap();
+			if lower+1 >= upper {
+				status = "Error";
+				// return status from here
+				return String::from(status);
+			}
+			// Getting the command to loop
+			print!(": ");
+			let mut command = String::from("");
+			let _=stdout().flush();
+			stdin().read_line(&mut command).expect("Error taking Input");
+
+			for i in lower..upper{
+				internal::set(args[0][1].clone(), String::from(i.to_string()), stack);
+				let execute = executer(command.clone(), &internal_commands, stack);
+				// Break if there is error
+				// if execute == "Error"{
+				// 	break;
+				// }
+				match execute {
+					Ok(r) => (),
+					Err(e) => break,
+				}
+			}
+			// Unset the variable after for loop is completed
+			internal::unset(args[0][1].clone(), stack);
+
+		}
+	}
+	// Return the status
+	String::from(status)
 }
 
+// This function takes care of parsing, distinguishing the command
+// and then execute it
+// It returns it execute state as string
 
-fn set_prompt(args: &Vec<&str>) -> String {
-	let mut pp = String::from("");
-	if args.len() == 1 {
-		pp.push_str("# ");
-	}
-	else {
-		for i in 1..args.len()-1 {
-			pp.push_str(args[i]);
-			pp.push(' ');
-		}
-		pp.push_str(args[args.len()-1]);
-	}
-	pp
-}
-
-
-fn main() {
-	use std::io::{stdin,stdout,Write};
-	let mut prompt = String::from("# ");
-	let mut internal_commands = Vec::new();
-
-	internal_commands.push("exit");
-	internal_commands.push("cd");
-	internal_commands.push("cwd");
-	internal_commands.push("echo");
-	internal_commands.push("mkdir");
-	internal_commands.push("cp");
-	internal_commands.push("rm");
-	internal_commands.push("mv");
-	internal_commands.push("grep");
-	internal_commands.push("cat");
-	internal_commands.push("ls");
-	internal_commands.push("date");
-	internal_commands.push("clear");
-	loop {
-		let mut command = String::from("");
-		print!("{}",prompt);
-		//ctrlc::set_handler(move || {
-		//	println!("received Ctrl+C!");
-		//}).expect("Error setting Ctrl-C handler");
-
-		let _=stdout().flush();
-		stdin().read_line(&mut command).expect("Invalid command");
-		command = String::from(command.trim());
-
-		command.push(' ');
-
-		let char_vec: Vec<char> = command.chars().collect();
-		let args_l = get_args_len(&char_vec).clone();
-		let mut args:Vec<&str> = Vec::new();
-		let mut start:usize = 0;
-
-		for _i in 0..args_l {
-			let (next, arg_start, arg_stop) = get_arg(&char_vec, start);
-			args.push(&&command[arg_start..arg_stop]);
-			start = next;
+fn executer(
+	command: String,
+	internal_commands: &Vec<String>,
+	stack: &mut HashMap<String, String>
+// ) -> Result<String, ()> {
+// ) -> () {
+) -> std::io::Result<()> {
+		let mut command = command;
+		for (key, value) in stack.clone() {
+			command = command.replace(&format!("${}", key),&value);
 		}
 
-		let mut internal = false;
+		let command = String::from(command);
 
-		if args[0] == "" {continue;}
-		if args[0] == "prompt"{
-			prompt = set_prompt(&args).clone();
-			continue;
+		let args_list = parser::parser(command);
+		let mut execute = "external";
+
+		let mut flag = 0;
+		if args_list.len() == 0{
+			// Ok(());
+			// return String::from(execute);
+			// return "No error".to_string()
+			// return Result<"NoError".to_string(), ()>
+			// Ok("No Error")
+			// Ok(());
+			flag = 1;
 		}
-		for com in &internal_commands{
-			if args[0] == *com {
-				internal = true;
-				run_internal(&args);
-				break;
+
+		if &args_list[0][0]==""{
+			// return String::from(execute);
+			// return Result<"NoError".to_string(), ()>
+			// return "No error".to_string()
+			// Ok()
+			// Ok("No Error")
+			// Ok(());
+			flag = 1;
+		}
+
+		if flag == 0{
+			let args_len = args_list.len();
+
+			for i in 0..args_len{
+				for com in internal_commands {
+					if &&args_list[i][0] == &com{
+						if args_len > 1 {
+							println!("Internal Commands cannot be piped");
+							execute = "Error";
+							break;
+						}
+						else {
+							execute = "internal";
+							break;
+						}
+					}
+				}
+				if execute!="external"{
+					break;
+				}
+			}
+
+			if execute=="internal" {
+				if &run_internal(args_list.clone(), stack, &internal_commands) == "Error"{
+					execute = "Error";
+				}
+			}
+			if execute=="external" {
+				spawner::pipe(args_list).expect("Error");
+				// let r1 = spawner::pipe(args_list);
+				// match r1 {
+				// 	Ok(r) => Ok(r),
+				// 	Err(e) => {
+				// 		println!("{:?}",e);
+				// 		Err(e)
+				// 	},
+				// };
 			}
 		}
-		if internal == false {
-			start_process(&args)
-		}
-	}
+		// Ok("No error".to_string())
+		Ok(())
+		// String::from(execute)
+}
 
+
+// This is the main loop that will take commands
+fn main() {
+	//Initiate the stack
+	let mut stack = internal::init_stack();
+	//Initiate the internal commands
+	let mut internal_commands = Vec::new();
+
+	internal_commands.push(String::from("exit"));
+	internal_commands.push(String::from("cd"));
+	internal_commands.push(String::from("clear"));
+	internal_commands.push(String::from("set"));
+	internal_commands.push(String::from("unset"));
+	internal_commands.push(String::from("prompt"));
+	internal_commands.push(String::from("for"));
+
+	// Loop for taking commands
+	// let mut r;
+	loop {
+		let mut command = String::from("");
+		print!("{}",stack["PROMPT"]);
+
+		let _=stdout().flush();
+		stdin().read_line(&mut command).expect("Error taking Input");
+
+		executer(command, &internal_commands, &mut stack).expect("Error");
+
+	}
 }
